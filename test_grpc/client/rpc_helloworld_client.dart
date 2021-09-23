5561a17b01c9714253abb379d58f3f635a7a07d8
harryng <little.ruan@gmail.com>
2021-09-24 01:27:31 +0700

    gRPC

--- test_grpc/client/rpc_helloworld_client.dart
+++ test_grpc/client/rpc_helloworld_client.dart
@@ -2,6 +2,7 @@ import 'package:grpc/grpc.dart';
 import 'package:learning_dart/test_grpc/gen/helloworld.pbgrpc.dart';
 
 Future<void> main(List<String> args) async {
+  // http://{{ grpc server address}}/{{proto package}}.{{proto service}}/{{method}}
   final channel = ClientChannel(
     '127.0.0.1',
     port: 50051,
@@ -28,3 +29,22 @@ Future<void> main(List<String> args) async {
   }
   await channel.shutdown();
 }
+// GET /page HTTP/1.1
+// Host: server.example.com
+// Connection: Upgrade, HTTP2-Settings
+// Upgrade: h2c 
+// HTTP2-Settings: (SETTINGS payload) 
+
+// HTTP/1.1 200 OK 
+// Content-length: 243
+// Content-type: text/html
+
+// (... HTTP/1.1 response ...)
+
+//           (or)
+
+// HTTP/1.1 101 Switching Protocols 
+// Connection: Upgrade
+// Upgrade: h2c
+
+// (... HTTP/2 response ...)
